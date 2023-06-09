35ca7891deebee810660e8c35afb7b73200485f5
Khijar Sheikh <120732221+Khijar21@users.noreply.github.com>
2023-06-09 17:21:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
-  //   write your code here
+	if(sampleObject.red === key){
+		return true;
+		break;
+	}
+	else if(sampleObject.green === key){
+		return true;
+		break;
+	}
+	else if(sampleObject.white === key){
+		return true;
+		break;
+	}
+	return false;
 }
 
 // Do not change the code below
 const key = prompt("Enter Key.");
 alert(hasKey(key));
+
